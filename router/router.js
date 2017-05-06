be43d7975ff849a9d2b961f78a9d90c15300c046
baek449 <baek449@gmail.com>
2017-05-06 18:46:12 +0900

    Initial Configuration
    Simple server that leading to dummy index and login page

---
+++ router/router.js
@@ -0,0 +1,9 @@
+module.exports = function(app)
+{
+     app.get('/',function(req,res){
+        res.render('index.html')
+     });
+     app.get('/login',function(req,res){
+        res.render('login.html');
+    });
+}
