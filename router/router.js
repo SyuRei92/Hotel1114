42378a4c1f0b23b40c3c190cfcb29de3972ee3a5
baek449 <baek449@gmail.com>
2017-05-08 23:29:34 +0900

    Mock login page
    Basic Login page without DB connection (always success)
    Implements session management and privilege level

--- router/router.js
+++ router/router.js
@@ -1,9 +1,31 @@
 module.exports = function(app)
 {
+	// Main Page - http://localhost:23400/
+	// Login Page - http://localhost:23400/login
+	// Member Page - http://localhost:23400/memberPage
+	// Error Page - http://localhost:23400/error
+	
+	require('./routerAccount')(app);
+
+	// Basic page render
      app.get('/',function(req,res){
-        res.render('index.html')
+        res.render('index.html');
      });
-     app.get('/login',function(req,res){
-        res.render('login.html');
-    });
+     
+     app.get('/error',function(req,res){
+    	 res.render('error.html');
+     });
+     
+     // Examples: Embed arguments in web page
+     app.get('/login1',function(req,res){
+        res.render('login2.html',{message:'myId'});
+     });
+     app.get('/login2/:id',function(req,res){
+    	// Access by http://localhost:23400/login2/myId2
+         res.render('login2.html',{message:req.params.id});
+     });
+     app.get('/login3',function(req,res){
+    	 // Access by http://localhost:23400/login3?id=myId3
+         res.render('login2.html',{message:req.query.id});
+     });
 };
